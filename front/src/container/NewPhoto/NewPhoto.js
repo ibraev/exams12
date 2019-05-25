12c69bdc9e02314a31f74e074a637229625f2908
Ibraev Iskender <amanibraev95@gmail.com>
2019-05-25 18:16:57 +0600

    Created New Form

--- front/src/container/NewPhoto/NewPhoto.js
+++ front/src/container/NewPhoto/NewPhoto.js
@@ -1,15 +1,26 @@
 import React, {Component, Fragment} from 'react';
 import PhotoForm from "../../components/PhotoForm/PhotoForm";
+import {createGallery} from "../../store/actions/actionsGallery";
+import {connect} from "react-redux";
 
 class NewPhoto extends Component {
+    createGallery = data => {
+        this.props.createGallery(data).then(() => {
+            this.props.history.push('/')
+        })
+    };
 
     render() {
         return (
             <Fragment>
-                <PhotoForm/>
+                <PhotoForm onSubmit={this.createGallery}/>
             </Fragment>
         );
     }
 }
 
-export default NewPhoto
+const mapDispatchToProps = dispatch => ({
+   createGallery: () => dispatch(createGallery())
+});
+
+export default connect(null, mapDispatchToProps) (NewPhoto)
