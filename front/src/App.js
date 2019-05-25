0409c618ef040980baeecf11333c176d275d02ca
Ibraev Iskender <amanibraev95@gmail.com>
2019-05-25 14:38:48 +0600

    Connected components via switch

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import {Container} from "reactstrap";
+import Routes from "./Routes";
 
 function App() {
   return (
     <div className="App">
-
+      <header>
+        <Toolbar/>
+      </header>
+      <Container>
+        <Routes/>
+      </Container>
     </div>
   );
 }
