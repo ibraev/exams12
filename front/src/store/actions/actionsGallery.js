6e978bb290e6a1eed9fb40f5d2da8d07c88ec6f5
Ibraev Iskender <amanibraev95@gmail.com>
2019-05-25 18:17:05 +0600

    Added scripts

--- front/src/store/actions/actionsGallery.js
+++ front/src/store/actions/actionsGallery.js
@@ -1,7 +1,8 @@
-import {FETCH_GALLERY_SUCCESS} from "./actionsType";
+import {CREATE_GALLERY_SUCCESS, FETCH_GALLERY_SUCCESS} from "./actionsType";
 import axios from '../../axios-api'
 
 export const fetchGallery_Success = gallery => ({type:FETCH_GALLERY_SUCCESS, gallery});
+export const createGallerySuccess = () => ({type:CREATE_GALLERY_SUCCESS });
 
 export const fetchGallery = (gallery) => {
     return dispatch => {
@@ -9,4 +10,13 @@ export const fetchGallery = (gallery) => {
             response => dispatch(fetchGallery_Success(response.data))
         )
     }
+};
+
+
+export const createGallery = productData => {
+    return (dispatch) => {
+        return axios.post('/gallery', productData).then(
+            () => dispatch(createGallerySuccess())
+        );
+    };
 };
