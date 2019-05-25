d8c5e888dd3db0ad3954a8573695c7b53f28d1ff
Ibraev Iskender <amanibraev95@gmail.com>
2019-05-25 17:43:15 +0600

    Created component with added new photo

---
+++ front/src/components/PhotoForm/PhotoForm.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {Button, Col, Form, FormGroup, Input} from "reactstrap";
+
+class PhotoForm extends Component {
+    state = {
+        title: '',
+        image:''
+    };
+
+    submitFormHandler = event => {
+        event.preventDefault();
+        const formData = new FormData();
+        Object.keys(this.state).forEach(key => {
+            formData.append(key, this.state[key]);
+        });
+        this.props.onSubmit(formData);
+    };
+    inputChangeHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    fileChangeHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.files[0]
+        })
+    };
+    render() {
+        return (
+            <Form>
+                <FormGroup row>
+                    <h2>New photo</h2>
+                    <Col sm={10}>
+                        <Input
+                            type="text" required
+                            name="title" id="title"
+                            value={this.state.title}
+                            onChange={this.inputChangeHandler}
+                        />
+                        <Input
+                            type="file" required
+                            name="image" id="image"
+                            value={this.state.image}
+                            onChange={this.inputChangeHandler}
+                        />
+
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Col sm={{offset:2, size: 10}}>
+                        <Button type="submit" color="primary">Save</Button>
+                    </Col>
+                </FormGroup>
+            </Form>
+        );
+    }
+}
+
+export default PhotoForm;
---
+++ front/src/container/NewPhoto/NewPhoto.js
@@ -0,0 +1,15 @@
+import React, {Component, Fragment} from 'react';
+import PhotoForm from "../../components/PhotoForm/PhotoForm";
+
+class NewPhoto extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <PhotoForm/>
+            </Fragment>
+        );
+    }
+}
+
+export default NewPhoto
